81c532bcb16abccda690a74fb86cb7e48de89c99
TravelReddy <50786766+TravelReddy@users.noreply.github.com>
2021-01-07 19:28:21 +0200

    Add files via upload

---
+++ detection_custom.py
@@ -0,0 +1,31 @@
+#================================================================
+#
+#   File name   : detection_custom.py
+#   Author      : PyLessons
+#   Created date: 2020-09-17
+#   Website     : https://pylessons.com/
+#   GitHub      : https://github.com/pythonlessons/TensorFlow-2.x-YOLOv3
+#   Description : object detection image and video example
+#
+#================================================================
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+import cv2
+import numpy as np
+import tensorflow as tf
+from yolov3.utils import detect_image, detect_realtime, detect_video, Load_Yolo_model, detect_video_realtime_mp
+from yolov3.configs import *
+
+# image_path   = "./guage_images/5.jpg"
+video_path   = "./IMAGES/test.mp4"
+
+yolo = Load_Yolo_model()
+
+for img in os.listdir('./guage_images/'):
+    image_path   = "./guage_images/"+img
+    image = detect_image(yolo, image_path, "./IMAGES/plate_1_detect.jpg", input_size=YOLO_INPUT_SIZE, show=False, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
+    cv2.imwrite('./Detected_Images/'+img,image)
+    # print(img)
+#detect_video(yolo, video_path, './IMAGES/detected.mp4', input_size=YOLO_INPUT_SIZE, show=False, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
+#detect_realtime(yolo, '', input_size=YOLO_INPUT_SIZE, show=True, CLASSES=TRAIN_CLASSES, rectangle_colors=(255, 0, 0))
+#detect_video_realtime_mp(video_path, "Output.mp4", input_size=YOLO_INPUT_SIZE, show=True, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0), realtime=False)
